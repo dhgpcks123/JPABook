d8d7a0b4163034d70a34e48f010915bd73961204
hyechan <dhgpcks@gmail.com>
2021-05-30 13:49:01 +0900

    @Embeddable protected 생성자와 사용

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,17 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 //어딘가에 내장될 수 있다.
-@Getter
+@Getter //Setter없이!
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){}
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
