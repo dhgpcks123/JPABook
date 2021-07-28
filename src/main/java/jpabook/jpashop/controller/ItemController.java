63132b100bde45bfd6eed4d1679b02c3c89ee31c
hyechan <dhgpcks123@naver.com>
2021-07-28 19:18:16 +0900

    상품 수정

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -6,9 +6,7 @@ import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +43,39 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items",items);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Book item = (Book)itemService.findOne(itemId);
+        BookForm form = new BookForm();
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setId(item.getId());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable String itemId){
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        //option x2 multiselect
+        //option -> smart move
+        //command shift u  upper
+        itemService.saveItem(book);
+        return "redirect:/items";
+
     }
 }
