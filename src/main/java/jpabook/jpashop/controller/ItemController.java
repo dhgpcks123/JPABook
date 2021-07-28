4ae28daf748172ebb54f19617f857af0cd47daa9
hyechan <dhgpcks123@naver.com>
2021-07-28 18:42:13 +0900

    상품 등록 구현

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/itmes/new")
+    public String create(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        //이거 setter를 다 제거하는 게 더 좋은 설정
+        // create해서 생성자처럼 쓸 수 있게 만들 수도 있을거고...
+        // 아니면 modelMapper쓸 수도 있는 듯?
+        // 엔티티 setter
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
